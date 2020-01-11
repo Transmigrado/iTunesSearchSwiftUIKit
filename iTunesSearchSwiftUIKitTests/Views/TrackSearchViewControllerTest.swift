ff2a880cfb1a03a3577bd76cbb512fad398cf7eb
transmigrado <transmigrado@gmail.com>
2020-01-11 16:43:58 -0300

    add test for SearchTableViewController

---
+++ iTunesSearchSwiftUIKitTests/Views/TrackSearchViewControllerTest.swift
@@ -0,0 +1,21 @@
+//
+//  TrackSearchViewControllerTest.swift
+//  iTunesSearchSwiftUIKitTests
+//
+//  Created by Jorge Acosta on 11-01-20.
+//  Copyright © 2020 Jorge Acosta. All rights reserved.
+//
+
+
+import Quick
+import Nimble
+
+class TableOfContentsSpec: QuickSpec {
+  override func spec() {
+    describe("Track Search") {
+        it("refresh table when change model") {
+            
+        }
+    }
+  }
+}
