1426fe6459d23fc7d908169f1feb430f65404490
transmigrado <transmigrado@gmail.com>
2020-01-11 15:14:24 -0300

    add cocoapods

---
+++ Pods/Target Support Files/Pods-iTunesSearchSwiftUIKit/Pods-iTunesSearchSwiftUIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iTunesSearchSwiftUIKit : NSObject
+@end
+@implementation PodsDummy_Pods_iTunesSearchSwiftUIKit
+@end
---
+++ Pods/Target Support Files/Pods-iTunesSearchSwiftUIKitTests/Pods-iTunesSearchSwiftUIKitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iTunesSearchSwiftUIKitTests : NSObject
+@end
+@implementation PodsDummy_Pods_iTunesSearchSwiftUIKitTests
+@end
